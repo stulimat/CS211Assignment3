16fea835717550f3722e75d759c01b70a318800e
Catalino <ccuadrado@gmail.com>
2012-02-12 18:56:41 -0500

    Initial commit

---
+++ Sorter.java
@@ -0,0 +1,219 @@
+import java.lang.Comparable;
+import java.util.LinkedList;
+
+/**
+ * Class sorter contains sorting methods for both LinkedLists and arrays
+ * @author Sam Tulimat 
+ **/ 
+
+public class Sorter
+{ 
+/**
+ * SelectionSort methods sorts comparable elements in an array 
+ * @param Comparable [] 
+ * @return Comparable [] array 
+ **/
+
+  public static Comparable[] selectionSort(Comparable[] array)
+  {
+    //instance variables 
+    int minValue;
+    Comparable innerValue;
+
+    //traverses the array to set the the minValue as the outer 
+    for (int outer = 0; outer < array.length; outer++)
+    {
+      minValue = outer;
+
+      //traverses the array comparing the minValue to the innerValue 
+      for( int inner = outer + 1; inner < array.length; inner++)
+      {
+        //if inner value is not the minValue 
+        if (array[inner].compareTo(array[minValue]) < 0)
+        {
+          //sets the inner to be the MinValue
+          minValue = inner;
+        }    
+      }
+      //if the first value is not the minValue
+      if (outer != minValue) 
+      {
+        //swaps the outer value with the minValue
+        array = swap(array,outer,minValue);
+      }
+
+    }
+    //returns the array 
+    return array;
+  } 
+
+  public static Comparable[] mergeSort(Comparable[] array)
+  {
+    //Check to see if the list is 1 element or smaller. If so, consider list sorted and return
+    if (array.length <= 1) 
+    {
+      return array;
+    }
+    else 
+    {
+       int middle = array.length / 2;
+       Comparable[] left = new Comparable[middle-1];
+       Comparable[] right = new Comparable[middle];
+       for(int index = 0; index < middle; index++)
+       {
+         left[index] = array[index];
+       }
+       for(int index = middle; index < array.length; index++)
+       {
+         right[index] = array[index];
+       }
+       left = mergeSort(left);
+       right = mergeSort(right);
+       return merge(left,right);
+    }
+  }
+
+  private static Comparable[] merge(Comparable[] left, Comparable[] right)
+  {
+    Comparable[] result = new Comparable[left.length + right.length];
+    int resultIndex = 0;
+    int leftIndex = 0;
+    int rightIndex = 0;
+    while(leftIndex < left.length && rightIndex < right.length)
+    {
+      if(left[leftIndex].compareTo(right[rightIndex]) <= 0)
+      {
+        result[resultIndex] = left[leftIndex];
+        leftIndex++;
+      }
+      else
+      {
+        result[resultIndex] = right[resultIndex];
+        rightIndex++;
+      }
+      resultIndex++;
+    }
+    
+    Comparable remainder[];
+    int remainderIndex = 0;
+
+    if(leftIndex >= left.length)
+    {
+      remainder = right;
+      remainderIndex = rightIndex;
+    }
+    else
+    {
+      remainder = left;
+      remainderIndex = leftIndex;
+    }
+    //Copy the rest of the array
+    for(int index = remainderIndex; index < remainder.length; index++)
+    { 
+      result[resultIndex] = remainder[remainderIndex];
+      resultIndex++;
+    }
+    
+    return result;
+  }
+ /**
+  * Sorts an array by insertionSort 
+  * @param Comparable[] array 
+  * @return Comparable[]
+  **/
+  public static Comparable[] insertionSort(Comparable[] array)
+  { 
+    //instance variable flag 
+    boolean flag = true;
+    // while the flag is false 
+    while(flag  == false);
+    { 
+      // sets the flag as false
+      flag = false;
+      
+      //traverses the array from index 1  
+      for (int i=1; i < array.length; i++) 
+      { 
+      //traverses the array back 
+        for (int j = i-1; ( j >= 0 ); j-- ) 
+        {
+          
+          if((array[j].compareTo(array[i] ) > 0 ))
+          {
+            System.out.println(array[j].toString()+" is greater than"+ array[i].toString());
+            array = swap(array,j+1,j);
+            flag = true;
+            break;
+          }
+        }
+        if(flag)
+        {
+          break;
+        }
+      }
+    }
+    return array; 
+  }
+  /**
+   * BubbleSort method to sort the array 
+   *@param Comparable [] array 
+   *@return Comparable[]
+   **/
+  public static Comparable[] bubbleSort(Comparable[] array)
+  {
+    boolean swapped; 
+    swapped = true;
+    Comparable valueOne;
+    Comparable valueTwo;
+    while(swapped == false)
+    {
+      swapped = false;
+      for(int i = 1; i < (array.length -1); i++)
+      {
+        valueOne= array[i-1];
+        valueTwo = array[i];
+        if(valueOne.compareTo(valueTwo) < 0 )
+        {
+          array = swap(array,i-1,i);
+          swapped = true;
+        }
+      }
+    }
+    return array;
+  }
+
+  public static void selectionSort(LinkedList<Comparable> list)
+  {
+    
+  }
+  
+  public static void insertionSort(LinkedList<Comparable> list)
+  {
+
+  }
+
+  public static LinkedList<Comparable> bubbleSort(LinkedList<Comparable> list)
+  {
+  
+    return list;
+  }
+
+
+  /**
+   * Swap method that swaps between two indexes in the array
+   * @param Comparable[] array , @param indexOne , @param indexTwo 
+   **/
+  private static Comparable[] swap( Comparable[] array, int indexOne, int indexTwo)
+  {
+    Comparable temp;
+    //temprorary value holding the indexOne of the array 
+    temp = array[indexOne];
+    //sets the indexTwo of the array as the IndexOne
+    array[indexOne] = array[indexTwo];
+    //sets the temp value to be the new indexTwo
+    array[indexTwo] = temp;
+    //returns the array 
+    return array;
+  }
+}
+      
---
+++ SorterTester.java
@@ -0,0 +1,163 @@
+import java.lang.Integer;
+import java.util.ListIterator;
+import java.util.LinkedList;
+import java.util.List;
+public class SorterTester
+{
+  LinkedList<Integer> listInsertionSort;
+  LinkedList<Integer> listBubbleSort;
+  LinkedList<Integer> listSelectionSort;
+  Integer[] arrayInsertionSort;
+  Integer[] arrayBubbleSort;
+  Integer[] arraySelectionSort;
+
+  public SorterTester(LinkedList<Integer> unsortedList)
+  {
+    this.listInsertionSort = unsortedList;
+    this.listBubbleSort = unsortedList;
+    this.listSelectionSort = unsortedList; 
+  }
+
+  public SorterTester(Integer[] unsortedArray)
+  {
+    this.arrayInsertionSort = unsortedArray;
+    this.arrayBubbleSort = unsortedArray;
+    this.arraySelectionSort = unsortedArray;
+  }
+
+  public static void testBubbleSort(LinkedList<Comparable> unsortedList)
+  {
+     List<Comparable> sortedList = new LinkedList<Comparable>();
+     //Run a bubble sort on the list. 
+     sortedList = Sorter.bubbleSort(unsortedList);
+     //Now verify that the sortedList is in sorted order somehow
+     ListIterator<Comparable> litr = sortedList.listIterator();
+     int flag = 0;
+     
+     while(litr.hasNext())
+     {
+       Integer i;
+       Integer iPlusOne;
+
+       i = (Integer) litr.next();
+       if(litr.hasNext())
+       {
+         iPlusOne = (Integer) litr.next();
+       }
+       else
+       {
+         break;
+       }
+
+       if(i.compareTo(iPlusOne) < 1 )
+       {
+              // Do nothing
+       }
+       else
+       {
+         flag = 1;
+         break; 
+       }
+         
+         //Do some stuff to the listIterator
+         litr.previous();
+      
+     }
+     if (flag == 1) 
+     {
+       System.out.println("Bubble Sort Test Failed");
+     }
+     else 
+     { 
+       System.out.println("Bubble sort test passed");
+     }
+     
+  }
+
+  public static void  testSelectionSort()
+  {
+    Integer[] passArray = {1,2,3,4,5,6,7};
+    Integer[] failArray = {5,9,7,2,6,8,4};
+    System.out.println("Testing Failing Selection Sorted Unsorted Array");
+    testSort(failArray);
+    System.out.println("Testing Passing Selection Sorted Unsorted Array");
+    testSort(passArray);
+    System.out.println("Testing Passing Selection Sorted Array");
+    testSort(Sorter.selectionSort(passArray));
+    System.out.println("Testing Failing Selection Sorted Array");
+    testSort(Sorter.selectionSort(failArray));
+  
+  }
+
+  public static void  testInsertionSort()
+  {
+    Integer[] passArray = {1,2,3,4,5,6,7};
+    Integer[] failArray = {5,9,7,2,6,8,4};
+    System.out.println("Testing Failing Insertion  Sorted Unsorted Array");
+    testSort(failArray);
+    System.out.println("Testing Passing Insertion Sorted Unsorted Array");
+    testSort(passArray);
+    System.out.println("Testing Passing Insertion Sorted Array");
+    testSort(Sorter.insertionSort(passArray));
+    System.out.println("Testing Failing Insertion Sorted Array");
+    testSort(Sorter.insertionSort(failArray));
+  }
+ 
+  public static void testSort(Comparable[] array)
+  {
+    boolean flag = false; 
+    for(int i = 0; i < (array.length - 1); i ++)
+    {
+      if(array[i].compareTo(array[i+1])>= 0)
+      {
+        flag = true;
+        break;
+      }
+      System.out.println(array[i].toString() +',');
+    }
+    if(flag)
+    {
+        System.out.println("Sort Test failed");
+    }
+    else
+    {
+       System.out.println("Sort Test passed");
+    }
+  }
+
+
+  public static void testInsertionSort(LinkedList<Integer> unsortedList)
+  {
+    //Implement like above. 
+  }
+
+  public static void main(String[] args)
+  {
+    LinkedList<Comparable> testList = new LinkedList<Comparable>();
+    LinkedList<Comparable> failList = new LinkedList<Comparable>();
+    Comparable[] passArray = {1,2,3,4,5,6};
+    Comparable[] failArray = {2,3,4,1,5,6};
+
+    testList.addFirst(4);
+    testList.addFirst(3);
+    testList.addFirst(2);
+    testList.addFirst(1);
+  
+    failList.addFirst(2);
+    failList.addFirst(5);
+    failList.addFirst(1);
+    failList.addFirst(7);
+    System.out.println("Testing Bubble Sort----");
+    System.out.println("Testing Passing List");
+    testBubbleSort(testList);
+    System.out.println("Testing Failing List");
+    testBubbleSort(failList);
+    System.out.println("Testing passing array");
+    testSort(passArray);
+    System.out.println("Testing failing array");
+    testSort(failArray);
+
+    testSelectionSort();
+    testInsertionSort();
+  }
+}
